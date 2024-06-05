3b6f47fda833b3c5defb1e5bc59fceca98c70be2
MouleFarcie <quent1richard@yahoo.com>
2024-06-05 16:20:03 +0200

    commit de merde

--- jeu.py
+++ jeu.py
@@ -5,7 +5,7 @@ def update():
     if pyxel.btn(pyxel.KEY_SPACE):
         print("connardouhiughiuh")
         print("test")
-        print("j'en pas ai marre")
+        print("j'en pas ai marre erighergkuygeuyerhguydehgueguerhiuergh")
 
 def draw():
     pyxel.rect(0,0,3,3,9)
