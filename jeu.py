7582a8d93292857a9c5a49c4745a1bdecd805176
MouleFarcie <quent1richard@yahoo.com>
2024-06-06 09:49:45 +0200

    commit 12

--- jeu.py
+++ jeu.py
@@ -1,21 +1,26 @@
 import pyxel
 import math
-pyxel.init(128, 128, title="NDC 2024")
+pyxel.init(256, 256, title="NDC 2024")
 pyxel.load("theme2.pyxres")
 
 #joueur
-px = 64
-py = 64
+px = 120
+py = 0
 
 #cam
 cpx = px-60
-
 cpy = py-60
 
 #clefs (3)
 clefs = [(0,10),(64,58),(100,12)]
 #coffre
 
+def hide():
+    global px, py
+    for i in range(px//8, px//8+17):
+        for j in range(16):
+            if math.sqrt((px+60-i*8)**2+(py+60-j*8)**2)>40:
+                pyxel.rect(i*8,j*8,8,8,0)
 
 def colli_joueur_clef():
     global clefs
@@ -37,11 +42,12 @@ def depv():
 def update():
     depv()
     colli_joueur_clef()
+    hide()
 
 def draw():
     pyxel.cls(3)
+    pyxel.bltm(0,0,0,0,0,256,256)
     pyxel.blt(px,py,0,32,0,16,16,0)
-    pyxel.bltm(0,0,0,0,0,128,128)
     pyxel.camera(cpx,cpy)
     for clef in clefs:
         pyxel.blt(clef[0],clef[1],0,0,16,8,16,0)
