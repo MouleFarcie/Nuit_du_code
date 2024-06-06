8e46805b42c18a0cbb7d6b377502e49e5f4dca3a
MouleFarcie <quent1richard@yahoo.com>
2024-06-06 11:27:38 +0200

    connard

--- jeu.py
+++ jeu.py
@@ -5,13 +5,15 @@ pyxel.init(128, 128, title="NDC 2024")
 pyxel.load("theme2.pyxres")
 
 #joueur
-px = 70
-py = 70
+px = 32
+py = 32
 nb_clefs = 0
 #cam
 cpx = px-60
 cpy = py-60
 
+capx = 0
+capy = 0
 #clefs (3)
 
 #coffre
@@ -20,10 +22,10 @@ tuiles_inter = [(1,0)]
 
 def hide():
     global px, py
-    for i in range(px//8, px//8+17):
-        for j in range(16):
-            if math.sqrt((px+60-i*8)**2+(py+60-j*8)**2)>40:
-                pyxel.rect(i*8,j*8,8,8,2)
+    for i in range(px//8-16, px//8+16):
+        for j in range(py//8-10, py//8+10):
+            if math.sqrt((px-i*8)**2+(py-j*8)**2)>40:
+                pyxel.rect(i*8,j*8,8,8,0)
 
 def colli_joueur_clef():
     global nb_clefs
@@ -63,17 +65,42 @@ def cam():
     if px>322:
         cpx = 256
 
+def carte():
+    global px, py, capx, capy
+    
+    capx = px-50
+    capy = py-50
+    
+    if px<=60:
+        capx = 10
+    
+    if py<=60:
+        capy = 10
+    
+    if px>=(384-62):
+        capx = 384-118
+    
+    if py>=(384-62):
+        capy = 384-118
+        
+        
+    
+
 def update():
     depv()
     colli_joueur_clef()
-    hide()
     cam()
     spawn_coffre()
-
+    carte()
+    
 def draw():
     pyxel.cls(3)
     pyxel.bltm(0,0,0,0,0,384,384)
+    hide()
     pyxel.blt(px,py,0,32,0,16,16,0)
+    pyxel.blt(capx,capy,0,0,32,16,16,0)
+    pyxel.blt((capx+1+px/32),(capy+1+py/32),0,16,32,1,1,0)
+
     pyxel.camera(cpx,cpy)
 
 pyxel.run(update,draw)
