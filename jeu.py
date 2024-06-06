8dadf523de8d0038d77fd8395038cbb6183b52c4
MouleFarcie <quent1richard@yahoo.com>
2024-06-06 08:18:25 +0200

    commit de base

--- jeu.py
+++ jeu.py
@@ -2,8 +2,8 @@ import pyxel
 pyxel.init(128, 128, title="NDC 2024")
 
 def update():
-    if pyxel.btn(pyxel.KEY_SPACE):
-        print("test")
+    if pyxel.btn(pyxel.KEY_Q):
+        pyxel.quit()
         
 def draw():
     pyxel.rect(0,0,3,3,9)
