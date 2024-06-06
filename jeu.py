e8f430bae52a87575e503d38143b3787e19ba36a
crevettee <h.guillaumont@gmail.com>
2024-06-06 09:04:11 +0200

    Henri

--- jeu.py
+++ jeu.py
@@ -5,10 +5,23 @@ pyxel.load("theme2.pyxres")
 px = 64
 py = 64
 
+def depv():
+    global px, py
+    if pyxel.btn(pyxel.KEY_RIGHT) and px < 120:
+        px += 1
+    if pyxel.btn(pyxel.KEY_LEFT) and px > 0:
+        px -= 1
+    if pyxel.btn(pyxel.KEY_DOWN) and py < 120:
+        py += 1
+    if pyxel.btn(pyxel.KEY_UP) and py > 0:
+        py -= 1
+
 def update():
     if pyxel.btn(pyxel.KEY_Q):
         pyxel.quit()
-        
+    
+    depv()
+
 def draw():
     pyxel.blt(px,py,0,0,0,16,16,0)
 
